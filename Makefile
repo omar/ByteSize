065e6f3c2f71cdab23abbdfbbf9ed4b00c32d61b
Omar <omar@users.noreply.github.com>
2016-06-10 04:03:11 -0500

    Fix #16: Modify nuspec dependencies and increment version number

--- Makefile
+++ Makefile
@@ -11,4 +11,3 @@ package:
 	xbuild src/ByteSizeLib/ByteSizeLib.csproj /p:Configuration=Release
 	xbuild src/ByteSizeLib/ByteSizeLib.Dotnet.csproj /p:Configuration=Release
 	mono tools/NuGet.exe Pack ByteSize.nuspec
-
--- src/ByteSizeLib/Properties/AssemblyInfo.cs
+++ src/ByteSizeLib/Properties/AssemblyInfo.cs
@@ -11,7 +11,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("ByteSize")]
-[assembly: AssemblyCopyright("Copyright © Omar Khudeira 2013-2014")]
+[assembly: AssemblyCopyright("Copyright © Omar Khudeira 2013-2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
@@ -26,5 +26,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.2.0.0")]
-[assembly: AssemblyFileVersion("0.2.0.0")]
+[assembly: AssemblyVersion("1.1.3.0")]
+[assembly: AssemblyFileVersion("1.1.3.0")]
