6bafc1743e22c13ec9670cd917c7a796f3239858
Omar <omar@users.noreply.github.com>
2016-11-25 00:25:13 -0600

    Prepare v1.2.4

--- src/ByteSizeLib/Properties/AssemblyInfo.cs
+++ src/ByteSizeLib/Properties/AssemblyInfo.cs
@@ -16,5 +16,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
 
-[assembly: AssemblyVersion("1.2.3")]
-[assembly: AssemblyInformationalVersion("1.2.3")]
+[assembly: AssemblyVersion("1.2.4")]
+[assembly: AssemblyInformationalVersion("1.2.4")]
