0502fe49a881813453e33ac3079f7503f662c2ef
omar <omar@users.noreply.github.com>
2021-11-04 19:22:42 -0500

    Document properties, fields, and methods

--- src/ByteSizeLib/BinaryByteSize.cs
+++ src/ByteSizeLib/BinaryByteSize.cs
@@ -5,22 +5,37 @@ namespace ByteSizeLib
 {
     public partial struct ByteSize
     {
+        /// <summary>Number of bytes in 1 kibibyte.</summary>
         public const long BytesInKibiByte = 1_024;
+        /// <summary>Number of bytes in 1 mebibyte.</summary>
         public const long BytesInMebiByte = 1_048_576;
+        /// <summary>Number of bytes in 1 gibibyte.</summary>
         public const long BytesInGibiByte = 1_073_741_824;
+        /// <summary>Number of bytes in 1 tebibyte.</summary>
         public const long BytesInTebiByte = 1_099_511_627_776;
+        /// <summary>Number of bytes in 1 pebibyte.</summary>
         public const long BytesInPebiByte = 1_125_899_906_842_624;
 
+        /// <summary>Kibibyte symbol.</summary>
         public const string KibiByteSymbol = "KiB";
+        /// <summary>Mebibyte symbol.</summary>
         public const string MebiByteSymbol = "MiB";
+        /// <summary>Gibibyte symbol.</summary>
         public const string GibiByteSymbol = "GiB";
+        /// <summary>Tebibyte symbol.</summary>
         public const string TebiByteSymbol = "TiB";
+        /// <summary>Pebibyte symbol.</summary>
         public const string PebiByteSymbol = "PiB";
 
+        /// <summary>Gets the number of kibibytes represented by this object.</summary>
         public double KibiBytes => Bytes / BytesInKibiByte;
+        /// <summary>Gets the number of mebibytes represented by this object.</summary>
         public double MebiBytes => Bytes / BytesInMebiByte;
+        /// <summary>Gets the number of gibibytes represented by this object.</summary>
         public double GibiBytes => Bytes / BytesInGibiByte;
+        /// <summary>Gets the number of tebibytes represented by this object.</summary>
         public double TebiBytes => Bytes / BytesInTebiByte;
+        /// <summary>Gets the number of pebibytes represented by this object.</summary>
         public double PebiBytes => Bytes / BytesInPebiByte;
 
         /// <inheritdoc cref="ByteSize.ByteSize(long)" />
@@ -104,6 +119,14 @@ namespace ByteSizeLib
             return this.ToString("0.##", CultureInfo.CurrentCulture, useBinaryByte: true);
         }
 
+        /// <summary>
+        /// Converts the value of the current object to a binary byte string.
+        /// The prefix symbol (bit, byte, kibi, mebi, etc.) used is the
+        /// largest prefix such that the corresponding value is greater than or
+        /// equal to one.
+        /// Use <see cref="ByteSize.ToString()"/> for decimal string representation.
+        /// </summary>
+        /// <param name="formatProvider">An object that supplies culture-specific formatting information.</param>
         public string ToBinaryString(IFormatProvider formatProvider)
         {
             return this.ToString("0.##", formatProvider, useBinaryByte: true);
--- src/ByteSizeLib/DecimalByteSize.cs
+++ src/ByteSizeLib/DecimalByteSize.cs
@@ -1,23 +1,40 @@
+using System;
+
 namespace ByteSizeLib
 {
     public partial struct ByteSize
     {
+        /// <summary>Number of bytes in 1 kilobyte.</summary>
         public const long BytesInKiloByte = 1_000;
+        /// <summary>Number of bytes in 1 megabyte.</summary>
         public const long BytesInMegaByte = 1_000_000;
+        /// <summary>Number of bytes in 1 gigabyte.</summary>
         public const long BytesInGigaByte = 1_000_000_000;
+        /// <summary>Number of bytes in 1 terabyte.</summary>
         public const long BytesInTeraByte = 1_000_000_000_000;
+        /// <summary>Number of bytes in 1 petabyte.</summary>
         public const long BytesInPetaByte = 1_000_000_000_000_000;
 
+        /// <summary>Kilobyte symbol.</summary>
         public const string KiloByteSymbol = "KB";
+        /// <summary>Megabyte symbol.</summary>
         public const string MegaByteSymbol = "MB";
+        /// <summary>Gigabyte symbol.</summary>
         public const string GigaByteSymbol = "GB";
+        /// <summary>Terabyte symbol.</summary>
         public const string TeraByteSymbol = "TB";
+        /// <summary>Petabyte symbol.</summary>
         public const string PetaByteSymbol = "PB";
 
+        /// <summary>Gets the number of kilobytes represented by this object.</summary>
         public double KiloBytes => Bytes / BytesInKiloByte;
+        /// <summary>Gets the number of megabytes represented by this object.</summary>
         public double MegaBytes => Bytes / BytesInMegaByte;
+        /// <summary>Gets the number of gigabytes represented by this object.</summary>
         public double GigaBytes => Bytes / BytesInGigaByte;
+        /// <summary>Gets the number of terabytes represented by this object.</summary>
         public double TeraBytes => Bytes / BytesInTeraByte;
+        /// <summary>Gets the number of petabytes represented by this object.</summary>
         public double PetaBytes => Bytes / BytesInPetaByte;
 
         /// <inheritdoc cref="ByteSize.ByteSize(long)" />
