7351ff231902609371ea2aed3839314419dd210c
Cédric Luthi <cedric.luthi@gmail.com>
2021-11-09 00:32:33 +0100

    Document EnumerableExtensions and its Sum method

--- src/ByteSizeLib/EnumerableExtensions.cs
+++ src/ByteSizeLib/EnumerableExtensions.cs
@@ -3,8 +3,16 @@ using System.Linq;
 
 namespace ByteSizeLib
 {
+    /// <summary>
+    /// Extension methods on sequence of <see cref="ByteSize"/> values.
+    /// </summary>
     public static class EnumerableExtensions
     {
+        /// <summary>
+        /// Computes the sum of a sequence of <see cref="ByteSize"/> values.
+        /// </summary>
+        /// <param name="byteSizes">A sequence of <see cref="ByteSize"/> values to calculate the sum of.</param>
+        /// <returns>The sum of the values in the sequence.</returns>
         public static ByteSize Sum(this IEnumerable<ByteSize> byteSizes)
         {
             return byteSizes.Aggregate((current, byteSize) => current + byteSize);
