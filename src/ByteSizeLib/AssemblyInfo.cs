183c56e220275c458b72549e858888d9c193cb23
SpReeDateD <webmaster@born2skate.de>
2023-05-27 15:28:03 +0200

    strong name signing

---
+++ src/ByteSizeLib/AssemblyInfo.cs
@@ -0,0 +1,19 @@
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// In SDK-style projects such as this one, several assembly attributes that were historically
+// defined in this file are now automatically added during build and populated with
+// values defined in project properties. For details of which attributes are included
+// and how to customise this process see: https://aka.ms/assembly-info-properties
+
+
+// Setting ComVisible to false makes the types in this assembly not visible to COM
+// components.  If you need to access a type in this assembly from COM, set the ComVisible
+// attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM.
+
+[assembly: Guid("c5d64482-f2bc-41b0-b6c1-0ed7a24f338d")]
+[assembly: InternalsVisibleTo("ByteSizeLib.Tests, PublicKey=00240000048000009400000006020000002400005253413100040000010001009dc5b9fd3ac54829a66893b2e69fa3bed6a91b4cc788c6a8a87f48e94210eb6728bac2cf673cc29f5e0d422c45970064e571af4b2b4f6c564fded8ab671f3a3c40c9667caa04c659c4d2aeef867eb75528db421ad5741b363cb03bba3bd28785a1a68edd70d86b653917c476b84145c4690da184fb58f2419f82f51e1ac14fcc")]
